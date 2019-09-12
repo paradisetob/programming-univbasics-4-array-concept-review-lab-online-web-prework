cb2e5aac95c3d53d6a4af3f8c45f4c2c59c5d4a8
tracey brimage <onelovenilynn@gmail.com>
2019-09-12 06:43:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,12 @@
 def find_element_index(array, value_to_find)
   scale = [2,3,4,5]
-  
+  scale.index[2]
 
 end
 
 def find_max_value(array)
     scale_2 = [6,5,4,3]
-  find_max_value("scale_2")
+  scale_2
 end
 
 def find_min_value(array)
