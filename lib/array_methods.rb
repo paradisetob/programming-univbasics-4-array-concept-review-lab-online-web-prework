fe29ff7d7f84f287e6e55dc3dcd9dd08f7c49441
tracey brimage <onelovenilynn@gmail.com>
2019-09-12 06:53:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,10 +6,10 @@ end
 
 def find_max_value(array)
     scale_2 = [6,5,4,3]
-  scale_2.max
+  scale_2.max(6)
 end
 
 def find_min_value(array)
     hill = [1,2,3,4,5,4,3,2,1]
-  hill.min 
+  hill.min(1)
 end
