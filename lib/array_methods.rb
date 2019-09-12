721df46609be95afe6ab32512403774e09d7f384
tracey brimage <onelovenilynn@gmail.com>
2019-09-12 06:55:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,15 @@
 def find_element_index(array, value_to_find)
   scale = [2,3,4,5]
-  scale.index(2)
+  scale.index ('2')
 
 end
 
 def find_max_value(array)
     scale_2 = [6,5,4,3]
-  scale_2.max(6)
+  scale_2.max ('6')
 end
 
 def find_min_value(array)
     hill = [1,2,3,4,5,4,3,2,1]
-  hill.min(1)
+  hill.min('1')
 end
